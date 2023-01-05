6a19c2b17007ca9e2137c39f3b5fd364339affbb
Ronald Rodrigues Pereira <57762848+RonMakaveli@users.noreply.github.com>
2023-01-05 13:25:41 -0300

    Upload Olá Mundo

---
+++ helloWorld/src/br/com/fiap/si/OlaMundo.java
@@ -0,0 +1,11 @@
+package br.com.fiap.si;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá Mundo");
+
+	}
+
+}
---
+++ helloWorld/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author TTC2478
+ *
+ */
+module helloWorld {
+}
